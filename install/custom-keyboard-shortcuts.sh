0fa8995a31e34d88560311ddfb703dd22468aa8d
mgdfp <morgangdfp@icloud.com>
2026-03-03 20:59:41 +0100

    Add custom keyboard shortcuts

---
+++ install/custom-keyboard-shortcuts.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Configuring custom keyboard shortcuts..."
+
+# 1. Unset the default 'home' key so it doesn't conflict
+gsettings set org.gnome.settings-daemon.plugins.media-keys home "[]"
+
+# 2. Reserve TWO custom shortcut slots (one for Flameshot, one for Explorer)
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']"
+
+# 3. Custom Slot 0: Flameshot Region (with Wayland fix)
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Flameshot Region'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'sh -c -- "flameshot gui"'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Super><Shift>s'
+
+# 4. Custom Slot 1: Force New File Explorer Window
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name 'New File Explorer'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command 'nautilus --new-window'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding '<Super>e'
